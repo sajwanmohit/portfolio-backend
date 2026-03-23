d22e8a495a23b9c2a26d6c0dbf76576b2850b00b
Mohit Sajwan <mohitsajwan0@gmail.com>
2026-03-23 10:58:08 +0530

    chore: prepare backend for deployment (env config + cors)

--- src/main/java/com/mohit/portfolio_backend/config/CorsConfig.java
+++ src/main/java/com/mohit/portfolio_backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Vite default
+                        .allowedOrigins("*") // Vite default
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
